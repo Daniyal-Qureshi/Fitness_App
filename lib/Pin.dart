89e165ec58bb0977083bd4757dc5963a5af4f34c
Daniyal Qureshi <daniyalqureshi268@gmail.com>
2023-09-16 16:50:22 -0400

    Designed and implemented login feature

---
+++ lib/Pin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Pin extends StatefulWidget {
+  const Pin({super.key});
+
+  @override
+  State<Pin> createState() => _PinState();
+}
+
+class _PinState extends State<Pin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
